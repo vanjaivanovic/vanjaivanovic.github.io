37755d973ffab6631d8e61341e41d974dda7239a
Vanja Ivanovic <vanja.i@hotmail.com>
2019-01-17 20:57:18 +0100

    first commit

---
+++ precache-manifest.7e35b34e14b693ba2444f94b89d95cd4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f4e04e58fc49b204356de0229d07cb2",
+    "url": "/static/media/flowers.1f4e04e5.jpg"
+  },
+  {
+    "revision": "13fc3138dab5cf44ce18",
+    "url": "/static/css/main.e5f81a49.chunk.css"
+  },
+  {
+    "revision": "a709d98a501dcd560f9c4915bc7a3a45",
+    "url": "/static/media/outsidecafe.a709d98a.jpg"
+  },
+  {
+    "revision": "ee578954aa561e20e99f",
+    "url": "/static/js/1.ee578954.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f7bc8a81b2f60b53eae7e301cc07d62",
+    "url": "/static/media/milkshakes.0f7bc8a8.jpg"
+  },
+  {
+    "revision": "f8a14ad1f71e2519380e0bb328027fb5",
+    "url": "/static/media/wraffle.f8a14ad1.jpg"
+  },
+  {
+    "revision": "5dacb7c6aa8a3c1f7aa50c16c326fe1c",
+    "url": "/static/media/waffle.5dacb7c6.jpg"
+  },
+  {
+    "revision": "c93730743b2a40480ca6757a6e451408",
+    "url": "/static/media/coffee.c9373074.jpg"
+  },
+  {
+    "revision": "adeb8b4bd229ca011d6bbfea85d3317a",
+    "url": "/static/media/whiteCake.adeb8b4b.jpg"
+  },
+  {
+    "revision": "13fc3138dab5cf44ce18",
+    "url": "/static/js/main.13fc3138.chunk.js"
+  },
+  {
+    "revision": "5f5973265f52f6c3d402a77bb44b4229",
+    "url": "/static/media/location1.5f597326.jpg"
+  },
+  {
+    "revision": "8293c87a5226572bb42879c3c133a320",
+    "url": "/static/media/dessert.8293c87a.jpg"
+  },
+  {
+    "revision": "29bd720ae48a31d79ee43b796bccf0a4",
+    "url": "/static/media/bread.29bd720a.jpg"
+  },
+  {
+    "revision": "9425cb583d33d0bcaf5007c6d0a77817",
+    "url": "/static/media/cake.9425cb58.jpg"
+  },
+  {
+    "revision": "4eb8465a9ab6ec56871c0a779a63c471",
+    "url": "/static/media/startpage.4eb8465a.jpg"
+  },
+  {
+    "revision": "e353abe2fdc2bc47db4ed8ec2e857ef5",
+    "url": "/static/media/menu1.e353abe2.jpg"
+  },
+  {
+    "revision": "1cfed1fb24dcff43883e7527230a1175",
+    "url": "/static/media/omOss1.1cfed1fb.jpg"
+  },
+  {
+    "revision": "ee578954aa561e20e99f",
+    "url": "/static/css/1.ce070588.chunk.css"
+  },
+  {
+    "revision": "5c1d5ef4cc31f910a0f62d185865de6d",
+    "url": "/index.html"
+  }
+];
